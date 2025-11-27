f09da04d0671b0c3d0b1863ccfa35abda5411616
Steven <s.coverdale@hotmail.com>
2025-11-27 16:08:51 -0500

    API work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState('Toronto');
+  const [weather, setWeather] = useState(null);
+  const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+      .then(res => res.json())
+      .then(data => setWeather(data));
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city"
+      />
+      {weather && weather.sys && (
+        <div>
+          <h2>{weather.name}, {weather.sys.country}</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>{weather.main.temp} °C</p>
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather icon" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
